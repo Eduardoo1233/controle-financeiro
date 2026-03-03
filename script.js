120b9f9f5159e2c2bf87c2a47091ae131cc7f68a
Eduardoo1233 <160926458+Eduardoo1233@users.noreply.github.com>
2026-03-03 14:50:15 -0300

    adiciona a função salvarTransacao e o envento de enviar as Informacoes da transação

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let transacoes = [];
+const descricaoTransacao = document.getElementById("descricaoTransacao");
+const valorTransacao = document.getElementById("valorTransacao");
+const btnEnviarInformacoes = document.getElementById("enviarDadosDaTransacao");
+
+function salvarTransacao() {
+    transacoes = [{
+        descricao: "descricaoTransacao", valor: "valorTransacao"
+    }];
+    conosole.log(transacoes);
+};
+
+btnEnviarInformacoes.addEventListener("click", salvarTransacao);
